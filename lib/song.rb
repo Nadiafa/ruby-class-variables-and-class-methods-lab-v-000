c3815a83f1fb19cb3cefef6a7957bf7e8e6861c6
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-21 09:19:37 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,35 +7,23 @@ class Song
 
   def initialize(name, artist, genre)
     @name = name
-    @artist = artist
-    @genre = genre
     @@count +=1
-    @@genres << genre
+    @artist = artist
     @@artists << artist
+    @genre = genre
+    @@genres << genre
   end
 
   def self.count
     @@count
   end
 
-  def self.artists
-    @@artists.uniq
-  end
-
-  def self.artist_count
-    artist_count = {}
-    @@artists.each do |artist|
-      if artist_count[artist]
-        artist_count[artist] += 1 
-      else
-        artist_count[artist] = 1
-      end
-    end
-    artist_count
-  end
-
   def self.genres
     @@genres.uniq
+  end
+  
+  def self.artists
+    @@artists.uniq
   end
 
   def self.genre_count
@@ -48,5 +36,17 @@ class Song
       end
     end
     genre_count
+  end
+  
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1 
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
   end
 end
