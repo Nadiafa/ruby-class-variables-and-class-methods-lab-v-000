ebd758c59b93218173c90400056fc39f0b6ea40e
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-21 09:17:54 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,32 +1,43 @@
-class Song 
+class Song
   attr_accessor :name, :artist, :genre
-    
-  @@count = 0 
+
+  @@count = 0
   @@artists = []
   @@genres = []
-  
-  def initialize
-    @@name = name
-    @@artist = artist
-    @@genre = genre
-    @@count += 1
-    @@artist_count += 1
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count +=1
     @@genres << genre
     @@artists << artist
   end
-    
+
   def self.count
-    @@count 
+    @@count
   end
-  
-  def self.genres
-    @@genres.uniq
-  end
-  
+
   def self.artists
     @@artists.uniq
   end
-  
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1 
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
   def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
@@ -38,19 +49,4 @@ class Song
     end
     genre_count
   end
-  
-  def self.artist_count
-    artist_count = {}
-    @@artists.each do |artist|
-      if artist_count[artist]
-        artist_count[artist] += 1 
-      else 
-        artist_count[artist] = 1 
-      end
-    end
-  end 
-    
-
-  
-  
-end 
+end
